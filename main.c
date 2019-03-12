04125435040f9edac4f0d122b95c7e08fed48a7b
Adam Zemanek <zemanek.adam98@gmail.com>
2019-03-12 13:56:35 +0100

    druha funkcia

--- main.c
+++ main.c
@@ -25,9 +25,32 @@ int zhoda(int A[], int b)
 	return pocitadlo;
 }
 
+void zjednotenie(int A[], int B[], int C[])
+{
+	int i, x=5, y;
+	
+	for(i=0;i<5;i++)
+	{
+		C[i]=A[i];
+	}
+	
+	for(i=0;i<5;i++)
+	{
+		zhoda(A, B[i]);
+		
+		if(y==0)
+		{
+			C[x]=B[i];
+			x++;
+		}
+	}
+}
+
+
+
 main()
 {
-	int A[5]={}, B[5]={};
+	int A[5]={}, B[5]={}, C[10]={};
 	nacitaj_pole(A);
 	nacitaj_pole(B);
 	
